81544963b628ebf60a62ca5c54f6c634dadb6bac
shazeen123 <shazeenshaheen2003@gmail.com>
2022-10-14 12:30:24 +0100

    First commit

--- median.py
+++ median.py
@@ -1,5 +1,5 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+
 
 while True:
     try:
@@ -10,3 +10,14 @@ while True:
     else:
         break
 print(numbers)
+
+
+list = [1, 3, 5, 2]
+list.sort()
+
+if list.__len__() % 2 == 0:
+    median = (list[list.__len__() / 2] + list[(list.__len__() - 2) / 2]) / 2
+else:
+    median = list[(list.__len__() - 1) / 2]
+
+print(median)
