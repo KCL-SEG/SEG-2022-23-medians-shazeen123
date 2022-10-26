ee8f4ca43150baee55164508e52833f92c972d91
shazeen123 <shazeenshaheen2003@gmail.com>
2022-10-26 15:52:40 +0100

    Solution

--- median.py
+++ median.py
@@ -9,16 +9,16 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+
+lower_position = int(len(numbers)/2) - 1
+upper_position = int(len(numbers)/2)
 
 
-list = [1, 3, 5, 2]
-list.sort()
-
-if list.__len__() % 2 == 0:
-    median = (list[list.__len__() / 2] + list[(list.__len__() - 2) / 2]) / 2
+if len(numbers) % 2 == 0:
+     median = (numbers[lower_position] + numbers[upper_position])/2
 else:
-    median = list[(list.__len__() - 1) / 2]
+    median = numbers[upper_position]
 
 print(median)
-//
